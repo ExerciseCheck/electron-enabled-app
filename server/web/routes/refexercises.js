6abbe6e2cb29312524753a5dac7d2aecc7460133
shreya <shreya.pandit25@gmail.com>
2017-10-28 19:08:31 -0400

    stringify json data

--- server/web/routes/refexercises.js
+++ server/web/routes/refexercises.js
@@ -84,7 +84,7 @@ internals.applyRoutes = function (server, next) {
         }
 
         return reply.view('refexercises/play', {
-          frameData: document["bodyFrames"]
+          frameData: JSON.stringify(document["bodyFrames"])
         });
     });
     }
