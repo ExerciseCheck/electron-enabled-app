52067a277fb66f89e37e281613fbbcbc81f04311
Rudhra Raveendran <rudytheninja@gmail.com>
2018-03-02 12:38:53 -0500

    charts

---
+++ server/web/routes/charts.js
@@ -0,0 +1,40 @@
+'use strict';
+const internals = {};
+const Config = require('../../../config');
+
+internals.applyRoutes = function (server, next) {
+
+  server.route({
+    method: 'GET',
+    path: '/charts',
+    config: {
+      auth: {
+        strategy: 'session'
+      }
+    },
+    handler: function (request, reply) {
+
+      return reply.view('charts/index', {
+        user: request.auth.credentials.user,
+        projectName: Config.get('/projectName'),
+        title: 'Charts',
+        baseUrl: Config.get('/baseUrl')
+      });
+    }
+  });
+
+  next();
+};
+
+
+exports.register = function (server, options, next) {
+
+  server.dependency(['auth'], internals.applyRoutes);
+
+  next();
+};
+
+exports.register.attributes = {
+  name: 'charts',
+  dependencies: 'visionary'
+};
