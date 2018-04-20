a3f3e600eea8265277b79b4384eca5455d748d67
asadeg02 <arezu.sadeghi@gmail.com>
2018-04-20 14:11:42 -0400

    Make requested changes

--- server/web/public/scripts/userexercise/session.js
+++ server/web/public/scripts/userexercise/session.js
@@ -1,42 +1,55 @@
 'use strict';
 
-function start() {
+function parseURL(url) {
 
-  var exerciseId ='';
-  var patientId = '';
-   
-  const url = window.location.pathname.split('/');
-  if (url.length === 6) {
-    exerciseId = url[4];
-    patientId = url[5];
-    window.location = '/userexercise/session/play/' + exerciseId +
-                      '/' + patientId;
-  }
-  else if (url.length === 5 ) {
-    exerciseId = url.pop();
-    window.location = '/userexercise/session/play/' + exerciseId;
-    
+  var exerciseId = null;
+  var patientId = null;
+  const urlToArray = url.split('/');
+  
+  //logged-in user is a patient
+  if (urlToArray.length === 6) {
+    exerciseId = urlToArray[4];
+    patientId = urlToArray[5];
   }
   
+  //logged-in user is a clinician
+  else if (urlToArray.length === 5) {
+    exerciseId = urlToArray.pop();
+    patientId = null;
+  }
+
+  return {
+    patientId: patientId,
+    exerciseId: exerciseId
+  };
 }
 
-function stop() {
 
-  var exerciseId ='';
-  var patientId = ''; 
+function action(action) {
 
-  const url = window.location.pathname.split('/');
-  if (url.length === 6) {
-    exerciseId = url[4];
-    patientId = url[5];
-    window.location = '/userexercise/session/stop/' + exerciseId +
-                      '/' + patientId;
+  const parsedURL = parseURL(window.location.pathname);
+  var patientId = parsedURL.patientId;
+  var exerciseId = parsedURL.exerciseId;
+   
+  if ( action === 'start') {
+    window.location = (parsedURL.patientId !== null)? 
+    '/userexercise/session/play/' + exerciseId + '/' + patientId:
+    '/userexercise/session/play/' + exerciseId;                                                  
   }
-  else if (url.length === 5 ) {
-    exerciseId = url.pop();
-    window.location = '/userexercise/session/stop/' + exerciseId;
-    
+
+  if ( action === 'discard' ) {
+    window.location = (parsedURL.patientId !== null)? 
+    '/userexercise/session/start/' + exerciseId + '/' + patientId:
+    '/userexercise/session/start/' + exerciseId;                                                  
+  } 
+ 
+
+  else if ( action === 'stop' ) {
+    window.location = (parsedURL.patientId !== null)? 
+    '/userexercise/session/stop/' + exerciseId + '/' + patientId:
+    '/userexercise/session/stop/' + exerciseId;             
   }
-}  
-  
-  
+}
+
+ 
+
