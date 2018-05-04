c152771a14ee1cfaa2097febac10e55bff48cab1
Frederick Jansen <frederick.jansen@gmail.com>
2018-05-04 13:55:19 -0400

    added electron as submodule

---
+++ anchor/server/web/public/scripts/index.js
@@ -0,0 +1,31 @@
+(function () {
+  let processing, canvas, ctx;
+  const colors = ['#ff0000', '#00ff00', '#0000ff', '#ffff00', '#00ffff', '#ff00ff'];
+
+  window.Bridge.startKinect = () => {
+    if (isElectron()) {
+      processing = false;
+      canvas = document.getElementById('outputCanvas');
+      ctx = canvas.getContext('2d');
+    }
+  };
+
+  window.Bridge.onBodyFrame = (bodyFrame) => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    let index = 0;
+    bodyFrame.bodies.forEach(function (body) {
+      if (body.tracked) {
+        for (let jointType in body.joints) {
+          let joint = body.joints[jointType];
+          ctx.fillStyle = colors[index];
+          ctx.fillRect(joint.depthX * 512, joint.depthY * 424, 10, 10);
+        }
+        index++;
+      }
+    });
+  };
+
+  function isElectron() {
+    return 'Bridge' in window;
+  }
+})();
