5741a821718c93f4555c22184ebead262ee77e08
EvenGu <yiweng@bu.edu>
2018-07-11 23:22:57 -0400

    add fields to exercise model

--- anchor/server/models/exercise.js
+++ anchor/server/models/exercise.js
@@ -10,6 +10,11 @@ class Exercise extends MongoModels {
       exerciseName,
       description,
       userId,
+      joint,
+      axis,
+      direction,
+      ref_lower_joint,
+      ref_upper_joint,
       createdAt: new Date()
     };
 
@@ -32,8 +37,13 @@ Exercise.schema = Joi.object().keys({
   _id: Joi.object(),
   exerciseName: Joi.string().required(),
   description: Joi.string().required(),
-  //this is the userId of the person creating the exersice 
+  //this is the userId of the person creating the exercise
   userId: Joi.string().required(),
+  joint: Joi.string().required(),
+  axis: Joi.string().required(),
+  direction: Joi.string().required(),
+  ref_lower_joint: Joi.string().required(),
+  ref_upper_joint: Joi.string().required(),
   createdAt: Joi.date().required()
 });
 
