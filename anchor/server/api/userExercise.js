65602e8d2ff2e165c3fcb6c824156561472a5492
shreya <shreya.pandit25@gmail.com>
2018-10-24 10:56:20 -0400

    Attempt to fix refftable call

--- anchor/server/api/userExercise.js
+++ anchor/server/api/userExercise.js
@@ -20,7 +20,7 @@ internals.applyRoutes = function (server, next) {
 //Will eventually get rid of because no longer need user exercises page but this should now load REF EX
   server.route({
     method: 'GET',
-    path: 'table/refexercise',
+    path: '/table/refexercise',
     config: {
       auth: {
         strategies: ['simple', 'jwt', 'session']
