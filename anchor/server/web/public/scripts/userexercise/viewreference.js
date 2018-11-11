46f612fe529a44975c83244a8a6576d2985f4642
Saurabh Singh <ssingh02@bu.edu>
2018-11-11 01:07:29 -0500

    Added button for saving raw data to file
    Signed-off-by: Saurabh Singh <ssingh02@bu.edu>

--- anchor/server/web/public/scripts/userexercise/viewreference.js
+++ anchor/server/web/public/scripts/userexercise/viewreference.js
@@ -1,5 +1,10 @@
 'use strict';
 
+function downloadData(bodyFrames) {
+  console.log(bodyFrames);
+
+}
+
 function deleteDoc(id) {
   const button = $('#delete' + id);
   if (button.text() === 'Delete') {
