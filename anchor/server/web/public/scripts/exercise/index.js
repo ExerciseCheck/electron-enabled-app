97180d53110a6c056a680e1534544aca793bb54e
Saurabh Singh <ssingh02@bu.edu>
2018-11-11 01:07:29 -0500

    Added button for saving raw data to file
    Signed-off-by: Saurabh Singh <ssingh02@bu.edu>

--- anchor/server/web/public/scripts/exercise/index.js
+++ anchor/server/web/public/scripts/exercise/index.js
@@ -1,5 +1,50 @@
 'use strict';
 
+// api/table/refexercise: all ref ex data
+
+
+function downloadData(id) {
+  $(this).val('clicked');
+  var exerciseData = [];
+  var exerciseDataString;
+  console.log(typeof exerciseData);
+  $.ajax({
+    type: 'GET',
+    url: '/api/table/userexercise/reference' + '/' + '5be60bf4efa3ed3cbcfe856b',
+    success: function (result) {
+      console.log("data=", result.data[0].exerciseId);
+      for ( var i=0; i<result.data.length; i++ )
+      {
+        if(result.data[i].exerciseId == id)
+        {
+          console.log("Found exercise", id);
+          // console.log(result.data[i].bodyFrames.length);
+          result.data[i].bodyFrames = JSON.parse(pako.inflate(result.data[i].bodyFrames, { to: 'string' }));
+          exerciseDataString = exerciseDataString + JSON.stringify(result.data[i].bodyFrames);
+          console.log(exerciseDataString.length)
+          // exerciseData.push(result.data[i].bodyFrames);
+          exerciseData.push(exerciseDataString);
+        }
+      }
+    },
+    error: function (result) {
+      errorAlert(result.responseJSON.message);
+    }
+  });
+
+  console.log("typeof:", typeof exerciseDataString);
+  // var blob = new Blob(exerciseData, {type: "text/plain;charset=utf-8"});
+  // var blob = new Blob(exerciseData[0], {type: "application/json"});
+    console.log(typeof String(exerciseDataString), String(exerciseDataString));
+    var blob = new Blob(String(exerciseDataString), {type: "text/plain;charset=utf-8"});
+    // var file = new File(String(exerciseDataString), Date()+".txt", {type: "text/plain;charset=utf-8"});
+    // saveAs(file)
+    saveAs(file, Date() + '.txt');
+
+  // saveAs(blob, "works.txt");
+  console.log("Done downloading!");
+}
+
 function deleteDoc(id) {
   const button = $('#delete' + id);
   if (button.text() === 'Delete') {
@@ -23,5 +68,3 @@ function deleteDoc(id) {
 function editDoc(id) {
   window.location = 'exercise/edit/' + id
 }
-
-
